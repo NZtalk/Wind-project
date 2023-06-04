cd7970aee9483b3abdfa04b4e184e2f16d12e809
Benjamin Guégan <benjamin.guegan@jellysmack.com>
2023-06-04 21:21:56 +0200

    add map with curves

--- src/dataviz_dash.py
+++ src/dataviz_dash.py
@@ -4,6 +4,7 @@ from dash.dependencies import Output, Input, State
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 from flask import Flask
+import dash_html_components as html
 
 server = Flask(__name__)
 app = dash.Dash(server=server, external_stylesheets=[dbc.themes.FLATLY])
@@ -11,14 +12,9 @@ app.title = 'Dashboard'
 
 
 
-app.layout = dcc.Dropdown(
-  id = 'dropdown',
-  options=[
-    {'label':'life expendancy', 'value': 'lifeExp'},
-    {'label':'population', 'value': 'pop'}
-    ],
-    value = 'pop', # Valeur affichée par défaut dans le menus.
-    multi = False # Spécifier si c'est un menu dropdown à multiple choix ou non.
+app.layout = html.Div([
+  html.H1('MAP'),
+  html.Iframe(id='map',srcDoc=open('src/turbines.html','r').read(),width='100%',height='600')]
 )
 if __name__ == '__main__' :
     app.run_server(debug=True, host='0.0.0.0')
