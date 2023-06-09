374b14eb267e62622a78a3585f6e3074f09e3b4c
NZ talks <nabil.zenaini2@gmail.com>
2023-06-09 19:53:58 +0200

    PIW

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends, Response
+from datetime import datetime
+from typing import List, Dict, Any
 from dotenv import load_dotenv
 import os
 from pymongo import MongoClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from bson import ObjectId
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from typing import List, Optional
-import mysql.connector
+from bson import ObjectId
+import json
+import requests
+from sqlalchemy.dialects.mysql import WKBElement
 
 app = FastAPI()
 
@@ -30,22 +35,24 @@ def mariadb_connection():
     password = os.environ["MARIADB_PASSWORD"]
     dbname = os.environ["MARIADB_DATABASE"]
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{user}:{password}@mariadb:3306/{dbname}'
-    eng = create_engine(SQLALCHEMY_DATABASE_URI).connect()
+    eng = create_engine(SQLALCHEMY_DATABASE_URI)
     return eng
 
 eng = mariadb_connection()
 
-class MongoModel(BaseModel):
-    id: Optional[str]
-
-class CoordModel(BaseModel):
+class Coord(BaseModel):
     lon: Optional[float]
     lat: Optional[float]
 
-class WeatherModel(BaseModel):
-    base: Optional[str]
 
-class MainModel(BaseModel):
+class Weather(BaseModel):
+    id: Optional[int]
+    main: Optional[str]
+    description: Optional[str]
+    icon: Optional[str]
+
+
+class Main(BaseModel):
     temp: Optional[float]
     feels_like: Optional[float]
     temp_min: Optional[float]
@@ -56,93 +63,154 @@ class MainModel(BaseModel):
     grnd_level: Optional[int]
     visibility: Optional[int]
 
-class WindModel(BaseModel):
+
+class Wind(BaseModel):
     speed: Optional[float]
     deg: Optional[int]
     gust: Optional[float]
 
-class CloudsModel(BaseModel):
+
+class Clouds(BaseModel):
     all: Optional[int]
 
-class SysModel(BaseModel):
+
+class Sys(BaseModel):
     type: Optional[int]
     id: Optional[int]
     country: Optional[str]
     sunrise: Optional[int]
     sunset: Optional[int]
     timezone: Optional[int]
-    name: Optional[str]
-    cod: Optional[int]
 
-class DataModel(BaseModel):
-    coord: Optional[CoordModel]
-    weather: Optional[List[WeatherModel]]
-    main: Optional[MainModel]
-    wind: Optional[WindModel]
-    clouds: Optional[CloudsModel]
-    dt: Optional[int]
-    sys: Optional[SysModel]
+
+class Data(BaseModel):
+    coord: Optional[Coord]
+    weather: Optional[List[Weather]]
+    main: Optional[Main]
+    wind: Optional[Wind]
+    clouds: Optional[Clouds]
+    dt: Optional[str]
+    sys: Optional[Sys]
     windfarm_id: Optional[str]
     name: Optional[str]
 
 class MongoDBResponse(BaseModel):
-    id: Optional[str] = Field(..., alias="id")
-    extract_date: Optional[str]
-    data: Optional[List[DataModel]]
+    id: Optional[str]
+    extract_date: datetime
+    data: List[Dict[str,Any]]
+
+    class Config:
+        json_encoders = {
+            ObjectId: Optional[str],
+            datetime: lambda v: v.isoformat(timespec='milliseconds') + 'Z'
+        }
 
 class ForecastDataModel(BaseModel):
-    coord: Optional[CoordModel]
-    weather: Optional[List[WeatherModel]]
-    main: Optional[MainModel]
-    wind: Optional[WindModel]
-    clouds: Optional[CloudsModel]
-    dt: Optional[int]
-    sys: Optional[SysModel]
+    coord: Optional[Coord]
+    weather: Optional[List[Weather]]
+    main: Optional[Main]
+    wind: Optional[Wind]
+    clouds: Optional[Clouds]
+    dt: Optional[datetime]
+    sys: Optional[Sys]
     windfarm_id: Optional[str]
     name: Optional[str]
     
-
-class ForescastModel(BaseModel):
-    id: Optional[str] = Field(..., alias="id")
-    extract_date: Optional[str]
-    data: Optional[List[ForecastDataModel]]
 
 def convert_objectid(obj):
     if isinstance(obj, ObjectId):
         return str(obj)
     return obj
 
+def sort_mongo_data(data):
+    sorted_data = sorted(data, key= lambda x : x['extract_date'])
+    return sorted_data
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        elif isinstance(obj, ObjectId):
+            return str(obj)
+        return super().default(obj)
+    
+
+   
 # Routes MongoDB
 
 @app.get("/mongodb/current")
-def get_mongodb_current():
+def get_mongodb_current(limit: int = 20, offset: int = 0):
     collection = mongodb_connection()["current"]
-    data = list(collection.find())
-    converted_data = [MongoDBResponse(**{**item, 'id': convert_objectid(item["_id"])}) for item in data]
-    return JSONResponse(content=jsonable_encoder(converted_data))
+    total_count = collection.count_documents({})  # Total count of documents
+
+    # Retrieve paginated data
+    data = list(collection.find().skip(offset).limit(limit))
+
+    sorted_data = sort_mongo_data(data)
+
+    response_data = MongoDBResponse(extract_date=datetime.now(), data=sorted_data)
+
+    content = json.dumps(response_data.dict(), cls=CustomJSONEncoder)
+    headers = {"Content-Type": "application/json"}
+
+    return Response(content=content, headers=headers)
+
 
 @app.get("/mongodb/forecast")
-def get_mongodb_current():
+def get_mongodb_forecast(limit: int = 20, offset: int = 0):
     collection = mongodb_connection()["forecast"]
-    data = list(collection.find())
-    converted_data = [ForescastModel(**{**item, 'id': convert_objectid(item["_id"])}) for item in data]
-    return JSONResponse(content=jsonable_encoder(converted_data))
+    total_count = collection.count_documents({})  # Total count of documents
+
+    # Retrieve paginated data
+    data = list(collection.find().skip(offset).limit(limit))
+
+    sorted_data = sort_mongo_data(data)
+
+    response_data = MongoDBResponse(extract_date=datetime.now(), data=sorted_data)
+
+    content = json.dumps(response_data.dict(), cls=CustomJSONEncoder)
+    headers = {"Content-Type": "application/json"}
+
+    return Response(content=content, headers=headers)
+
 
 @app.get("/mongodb/scada")
-def get_mongodb_current():
+def get_mongodb_scada(limit: int = 20, offset: int = 0):
     collection = mongodb_connection()["scada"]
-    data = list(collection.find())
-    converted_data = [MongoModel(**{**item, 'id': convert_objectid(item["_id"])}) for item in data]
+    total_count = collection.count_documents({})  # Total count of documents
+
+    # Retrieve paginated data
+    data = list(collection.find().skip(offset).limit(limit))
+
+    # Convert and organize the data
+    converted_data = []
+    for item in data:
+        converted_item = {}
+        for key, value in item.items():
+            if isinstance(value, ObjectId):
+                converted_item[key] = str(value)
+            elif isinstance(value, dict):
+                converted_item[key] = {sub_key: convert_objectid(sub_value) for sub_key, sub_value in value.items()}
+            else:
+                converted_item[key] = value
+        converted_data.append(converted_item)
+
     return JSONResponse(content=jsonable_encoder(converted_data))
 
-# Routes MariaDB
 
+
+# Routes MariaDB
 @app.get("/mariadb/powercurves")
 def get_mariadb_powercurves():
     with eng.connect() as connection:
-        result = connection.execute("SELECT * FROM powercurves")
-        data = result.fetchall()
-    return {"data": data}
+        query = text("SELECT * FROM powercurves")
+        result = connection.execute(query)
+        data = [dict(row) for row in result]
+
+        encoded_data = jsonable_encoder(data, custom_encoder={WKBElement: lambda _: None})
+    
+    return {"data": encoded_data}
+
 
 @app.get("/mariadb/windfarms")
 def get_mariadb_windfarms():
@@ -157,3 +225,16 @@ def get_mariadb_windturbines():
         result = connection.execute("SELECT * FROM windturbines")
         data = result.fetchall()
     return {"data": data}
+
+@app.get("/api/endpoint")
+def get_api_data():
+    response = requests.get('https://votre-api.com/endpoint')
+
+    if response.status_code == 200:
+        data = response.json()
+        converted_data = JSONResponse(content=jsonable_encoder(data))
+        return converted_data
+
+    else:
+        raise HTTPException(status_code=response.status_code, detail=f"La requête API a échoué avec le code {response.status_code}")
+
